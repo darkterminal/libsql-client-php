1b644aad5e19b4dad63dfaea1a6d2292dc5ce5ba
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-12 16:33:35 +0700

    feat: Add ExpandedScheme class
    Added a new class `ExpandedScheme` to the `Darkterminal\LibSQL\Types` namespace. This class provides constants representing expanded schemes for various protocols, including 'wss', 'ws', 'https', 'http', and 'file'.

---
+++ src/Types/ExpandedScheme.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Darkterminal\LibSQL\Types;
+
+/**
+ * Class ExpandedScheme
+ *
+ * The ExpandedScheme class provides constants representing expanded schemes for various protocols.
+ *
+ * @package Darkterminal\LibSQL\Types
+ */
+class ExpandedScheme
+{
+    public const wss = 'wss';
+    public const ws = 'ws';
+    public const https = 'https';
+    public const http = 'http';
+    public const file = 'file';
+}
