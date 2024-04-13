c91d875abb98a73d470bef2d7d083c8f1ccb942e
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-13 14:16:17 +0700

    feat: Add TransactionMode checker method
    Implemented a static method `checker` in the TransactionMode class located at src/Types/TransactionMode.php. This method validates the provided transaction mode against the available constants in the TransactionMode class. If the provided mode is not supported, it throws a LibsqlError with an appropriate message. This addition enhances the safety and robustness of transaction mode handling in the LibSQL library.

--- src/Types/TransactionMode.php
+++ src/Types/TransactionMode.php
@@ -2,6 +2,9 @@
 
 namespace Darkterminal\LibSQL\Types;
 
+use Darkterminal\LibSQL\Utils\Exceptions\LibsqlError;
+use ReflectionClass;
+
 /**
  * Class TransactionMode
  *
@@ -14,4 +17,15 @@ class TransactionMode
     public const write = 'write';
     public const read = 'read';
     public const deferred = 'deferred';
+
+    public static function checker(string $mode): string
+    {
+        $reflection = new ReflectionClass(TransactionMode::class);
+        $constants = $reflection->getConstants();
+        if (!in_array($mode, $constants)) {
+            throw new LibsqlError("Transaction mode is not supported. The only avaibale mode is: write, read, and deferred", "INVALID_TRANSACTION_MODE");
+        }
+
+        return $mode;
+    }
 }
