7f44f4cdca6c9849ef57cc11643ce4ae2a51b8fe
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-15 22:02:37 +0700

    chore: Add Logging trait for error message logging

--- src/Traits/Logging.php
+++ src/Traits/Logging.php
@@ -2,21 +2,37 @@
 
 namespace Darkterminal\LibSQL\Traits;
 
+/**
+ * Trait Logging
+ *
+ * This trait provides logging functionality for error messages.
+ */
 trait Logging
 {
-    public function log($message)
+    /**
+     * Log a message to the error log file.
+     *
+     * @param mixed $message The message to log.
+     *
+     * @return void
+     */
+    public function log($message): void
     {
         $filePath = $this->checkAndCreateDirectoryAndFile();
         $logMessage = '[' . date('Y-m-d H:i:s') . '] ' . $message . PHP_EOL;
         file_put_contents($filePath, $logMessage, FILE_APPEND);
     }
 
-    protected function checkAndCreateDirectoryAndFile()
+    /**
+     * Check and create the directory and error log file if they don't exist.
+     *
+     * @return string The path to the error log file.
+     */
+    protected function checkAndCreateDirectoryAndFile(): string
     {
         $homeDirectory = getenv('HOME');
         $directoryPath = $homeDirectory . '/.libsql-php';
         $filePath = $directoryPath . '/errors.log';
-
 
         if (!file_exists($directoryPath)) {
             mkdir($directoryPath, 0777, true);
@@ -29,3 +45,4 @@ trait Logging
         return $filePath;
     }
 }
+
