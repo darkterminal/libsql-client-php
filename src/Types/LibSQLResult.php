693e2bc5b3ea8e75cb43f0069e8f715f34356ef0
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-15 22:03:58 +0700

    feat: Add LibSQLResult class to represent different formats for fetching SQL results

---
+++ src/Types/LibSQLResult.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Class LibSQLResult
+ *
+ * Represents different formats for fetching SQL results.
+ */
+class LibSQLResult
+{
+    public const FETCH_ASSOC = 1;
+    public const FETCH_OBJ = 2;
+    public const FETCH_RAW = 3;
+}
