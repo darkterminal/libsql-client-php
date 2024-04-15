638da8f45a216c4048e7cf47a4e432398850ede7
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-04-15 22:01:01 +0700

    examples: Add transaction.php for demonstrating transaction management

---
+++ examples/transaction.php
@@ -0,0 +1,42 @@
+<?php
+
+use Darkterminal\LibSQL\LibSQL;
+use Darkterminal\LibSQL\Types\HttpStatement;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$config = [
+    'url' => 'libsql://127.0.0.1:8001',
+    'tls' => false
+];
+
+$db = new LibSQL($config);
+
+if ($db->connect()) {
+    echo "Connection Establised!" . PHP_EOL;
+}
+
+// Start a new transaction
+$transaction = $db->startTransaction();
+
+// Add the first SQL statement to the transaction
+if (true) {
+    $transaction->addTransaction(HttpStatement::create("UPDATE users SET name = 'Turso DB' WHERE id = 1"));
+} else {
+    // If a condition is not met, rollback the transaction and exit
+    $transaction->rollback();
+    exit();
+}
+
+// Add the second SQL statement to the transaction
+if (true) {
+    $transaction->addTransaction(HttpStatement::create("UPDATE users SET name = 'darkterminal' WHERE id = 2"));
+} else {
+    // If another condition is not met, rollback the transaction and exit
+    $transaction->rollback();
+    exit();
+}
+
+// End the transaction (commit changes)
+$result = $transaction->endTransaction();
+print_r($result);
